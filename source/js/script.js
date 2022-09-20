4dd98ba2f827b8328f7af189aeaa6ffff3ee4d67
Nikita <nsmirnov1@gmail.com>
2022-09-20 18:35:03 +0300

    Hometask 20/09

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,8 @@
 document.querySelector(".header-nav").classList.add("header-nav--js");
 document.querySelector(".header-nav__toggle").addEventListener("click", function(){
   document.querySelector(".header-nav").classList.toggle("header-nav--open")
-})
+});
+document.querySelector(".slider__handle").addEventListener("click", function(){
+  document.querySelector(".slider").classList.remove("slider--demo");
+  document.querySelector(".slider").classList.toggle("slider--before");
+});
